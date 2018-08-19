aef70af17d0cd8a7e5aa284f43ba495deb2a8936
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-08-19 16:57:22 +0900

    add qiita/tail_call_opt/fun.cmm

---
+++ qiita/tail_call_opt/call_cmm.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+	long ret;
+
+	__asm__(
+		"addq $-16, %% rsp\n\t"
+		"movq %%rbp, 8(%%rsp)\n\t"
+		"movq %%rsp, %% rbp\n\t"
+		"movq $print_ret, (%%rbp)\n\t"
+		"jmp foo\n"
+	"print_ret:\n\t"
+		"movq 8(%%rsp), %%rbp\n\t"
+		"addq $16, %%rsp\n"
+	: "=b"(ret)
+	:
+	: );
+
+	printf("ret: %ld\n", ret);
+	return 0;
+}
