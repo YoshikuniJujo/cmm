0c3d37c9618907ad6416f9399e27167c081322a6
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-08-22 09:39:39 +0900

    qiita: brainf-cmm: refactoring ParserCombinator

--- qiita/brainf-cmm/brainf-cmm/src/ParserCombinator.hs
+++ qiita/brainf-cmm/brainf-cmm/src/ParserCombinator.hs
@@ -1,53 +1,38 @@
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module ParserCombinator (Parse, parse) where
 
 import Control.Applicative (Alternative(..))
 import Data.Maybe (listToMaybe)
-import Text.Parser.Combinators
-import Text.Parser.Char
+import Text.Parser.Combinators (Parsing(..))
+import Text.Parser.Char (CharParsing(..))
 
 newtype Parse a = Parse { runParse :: String -> [(a, String)] }
 
 parse :: Parse a -> String -> Maybe a
 parse p = listToMaybe . map fst . ((p <* eof) `runParse`)
 
-succeed :: a -> Parse a
-succeed v = Parse $ \inp -> [(v, inp)]
-
-check :: (Char -> Bool) -> Parse Char
-check p = Parse $ \inp -> case inp of
-	c : cs | p c -> [(c, cs)]
-	_ -> []
-
 instance Functor Parse where
 	fmap f (Parse p) = Parse $ \inp -> [ (f x, r) | (x, r) <- p inp ]
 
 instance Applicative Parse where
+	pure v = Parse $ \inp -> [(v, inp)]
 	Parse pf <*> Parse px = Parse $ \inp ->
 		[ (f x, r') | (f, r) <- pf inp, (x, r') <- px r ]
-	pure = succeed
 
 instance Alternative Parse where
-	Parse p1 <|> Parse p2 = Parse $ \inp -> p1 inp ++ p2 inp
 	empty = Parse $ \_ -> []
-
-pEof :: Parse ()
-pEof = Parse $ \inp -> case inp of
-	"" -> [((), "")]
-	_ -> []
-
-pNotFollowedBy :: Show a => Parse a -> Parse ()
-pNotFollowedBy (Parse p) = Parse $ \inp -> case p inp of
-	[] -> [((), inp)]
-	_ -> []
+	Parse p1 <|> Parse p2 = Parse $ \inp -> p1 inp ++ p2 inp
 
 instance Parsing Parse where
 	try = id
 	(<?>) = const
-	notFollowedBy = pNotFollowedBy
-	unexpected = error
-	eof = pEof
+	unexpected _ = Parse $ \_ -> []
+	eof = Parse $ \case "" -> [((), "")]; _ -> []
+	notFollowedBy (Parse p) = Parse $ \inp -> case p inp of
+		[] -> [((), inp)]
+		_ -> []
 
 instance CharParsing Parse where
-	satisfy = check
+	satisfy p = Parse $ \case c : cs | p c -> [(c, cs)]; _ -> []
