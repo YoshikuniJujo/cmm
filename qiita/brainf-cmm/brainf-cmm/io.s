59211e26774a424ad4396282de04909d87fd862e
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-08-22 10:33:31 +0900

    qiita: brainf-cmm: refactoring io.s

---
+++ qiita/brainf-cmm/brainf-cmm/io.s
@@ -0,0 +1,23 @@
+	.global putchar_syscall
+putchar_syscall:
+	movb %bl, ch(%rip)
+	movq $1, %rax
+	movq $1, %rdi
+	movq $ch, %rsi
+	movq $1, %rdx
+	syscall
+	xorq %rbx, %rbx
+	jmp *(%rbp)
+
+	.global getchar_syscall
+getchar_syscall:
+	movq $0, %rax
+	movq $0, %rdi
+	movq $ch, %rsi
+	movq $1, %rdx
+	syscall
+	xorq %rbx, %rbx
+	movb ch(%rip), %bl
+	jmp *(%rbp)
+
+	.comm ch,1
