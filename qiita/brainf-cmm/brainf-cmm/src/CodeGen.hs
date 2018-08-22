54c380a72f94cc4f811a30bc9672876f0d7ce7ec
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-08-22 10:11:27 +0900

    qiita: brainf-cmm: refactoring CodeGen.hs

--- qiita/brainf-cmm/brainf-cmm/src/CodeGen.hs
+++ qiita/brainf-cmm/brainf-cmm/src/CodeGen.hs
@@ -3,49 +3,49 @@
 
 module CodeGen (codeGen) where
 
-import BrainfParser
-
-type FunName = String
-
-data OpCall = Op Op | Call FunName deriving Show
+import BrainfParser (ParseForest, ParseTree(..), Op(..), parseBrainf)
 
 codeGen :: String -> Maybe String
-codeGen = (funsToCode . toFunctions . ("fun" ,) <$>) . parseBrainf
+codeGen = (toCmm . toFunctions . ("fun" ,) <$>) . parseBrainf
 
-funsToCode :: [Function] -> String
-funsToCode (tf : fs) = header ++ topFun tf ++ concatMap loopFun fs
-funsToCode _ = error "funsToCode: no functions"
+data OpCall = Op Op | Call FunName deriving Show
+type FunName = String
+
+toCmm :: [Function] -> String
+toCmm (tf : fs) = header ++ topFun tf ++ concatMap loopFun fs
+toCmm _ = error "toCmm: no functions"
 
 header :: String
 header =
-	"section \"data\"\n" ++
-	"{\n\tmemory: bits8[30000];\n}\n\n" ++
-	"cmm_main()\n{\n\tR2 = memory;\n\t" ++
-	"(bits64 r) = call fun();\n\treturn(r);\n}\n\n"
+	"section \"data\"\n{\n\tmemory: bits8[30000];\n}\n\n" ++
+	"cmm_main()\n{\n\t" ++
+		"R2 = memory;\n\t" ++
+		"(bits64 r) = call fun();\n\t" ++
+		"return(r);\n}\n\n"
 
 topFun :: Function -> String
-topFun f = funName f ++ "()\n{\n" ++
-	concatMap toInstruction (funBody f) ++ "\t\treturn(bits8[R2]);\n}\n\n"
+topFun Function { funName = fn, funBody = fb } =
+	fn ++ "()\n{\n" ++
+		concatMap toInstr fb ++ "\t\treturn(bits8[R2]);\n}\n\n"
 
 loopFun :: Function -> String
-loopFun f = funName f ++ "()\n{\n" ++
-	intoLoop (funName f) (concatMap toInstruction $ funBody f) ++ "}\n\n"
+loopFun Function { funName = fn, funBody = fb } =
+	fn ++ "()\n{\n" ++ intoLoop fn (concatMap toInstr fb) ++ "}\n\n"
 
 intoLoop :: FunName -> String -> String
-intoLoop f ops =
-	"\tif (bits8[R2] > 0) {\n" ++ ops ++
-	"\t\tjump " ++ f ++ "();\n\t" ++
+intoLoop fn ops =
+	"\tif (bits8[R2] > 0) {\n" ++ ops ++ "\t\tjump " ++ fn ++ "();\n\t" ++
 	"} else {\n\t\treturn();\n\t}\n"
 
-toInstruction :: OpCall -> String
-toInstruction (Op PtrInc) = "\t\tif (R2 < memory + 29999) { R2 = R2 + 1; }\n"
-toInstruction (Op PtrDec) = "\t\tif (R2 > memory) { R2 = R2 - 1; }\n"
-toInstruction (Op ValInc) = "\t\tbits8[R2] = bits8[R2] + 1;\n"
-toInstruction (Op ValDec) = "\t\tbits8[R2] = bits8[R2] - 1;\n"
-toInstruction (Op PutCh) = "\t\tcall putchar_syscall(bits8[R2]);\n"
-toInstruction (Op GetCh) =
+toInstr :: OpCall -> String
+toInstr (Op PtrInc) = "\t\tif (R2 < memory + 29999) { R2 = R2 + 1; }\n"
+toInstr (Op PtrDec) = "\t\tif (R2 > memory) { R2 = R2 - 1; }\n"
+toInstr (Op ValInc) = "\t\tbits8[R2] = bits8[R2] + 1;\n"
+toInstr (Op ValDec) = "\t\tbits8[R2] = bits8[R2] - 1;\n"
+toInstr (Op PutCh) = "\t\tcall putchar_syscall(bits8[R2]);\n"
+toInstr (Op GetCh) =
 	"\t\t(bits8 r) = call getchar_syscall(); bits8[R2] = r;\n"
-toInstruction (Call fn) = "\t\tcall " ++ fn ++ "();\n"
+toInstr (Call fn) = "\t\tcall " ++ fn ++ "();\n"
 
 data Function = Function { funName :: FunName, funBody :: [OpCall] }
 	deriving Show
@@ -53,14 +53,12 @@ data Function = Function { funName :: FunName, funBody :: [OpCall] }
 toFunctions :: (FunName, ParseForest) -> [Function]
 toFunctions (fn, pf) = Function fn ops : concatMap toFunctions fnpfs
 	where
-	(ops, fnpfs) =
-		popFunction (map (((fn ++ "_") ++) . show) [(1 :: Int) ..]) pf
+	(ops, fnpfs) = popFun (map (((fn ++ "_") ++) . show) [(1 :: Int) ..]) pf
 
-popFunction :: [FunName] -> ParseForest -> ([OpCall], [(FunName, ParseForest)])
-popFunction fns (PtNop : pf) = popFunction fns pf
-popFunction (fn : fns) (PtLoop l : pf) = (Call fn : ops, (fn, l) : ls)
-	where (ops, ls) = popFunction fns pf
-popFunction fns (PtOp op : pf) = (Op op : ops, ls)
-	where (ops, ls) = popFunction fns pf
-popFunction _ [] = ([], [])
-popFunction [] (PtLoop _ : _) = error "popFunction: function name missing"
+popFun :: [FunName] -> ParseForest -> ([OpCall], [(FunName, ParseForest)])
+popFun fns (PtNop : pf) = popFun fns pf
+popFun (fn : fns) (PtLoop l : pf) =
+	let (ops, ls) = popFun fns pf in (Call fn : ops, (fn, l) : ls)
+popFun fns (PtOp op : pf) = let (ops, ls) = popFun fns pf in (Op op : ops, ls)
+popFun _ [] = ([], [])
+popFun [] (PtLoop _ : _) = error "popFun: Not enough function names"
