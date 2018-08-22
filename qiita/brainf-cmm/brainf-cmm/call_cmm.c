dd229ac65c5555ac13c6a87ec1ca442353ac5b3d
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-08-22 10:46:44 +0900

    qiita: brainf-cmm: not put return value from cmm

--- qiita/brainf-cmm/brainf-cmm/call_cmm.c
+++ qiita/brainf-cmm/brainf-cmm/call_cmm.c
@@ -3,8 +3,6 @@
 int
 main(int argc, char *argv[])
 {
-	long ret;
-
 	__asm__(
 		"addq $-16, %%rsp\n\t"
 		"movq %%rbp, 8(%%rsp)\n\t"
@@ -14,10 +12,9 @@ main(int argc, char *argv[])
 	"ret:\n\t"
 		"movq 8(%%rsp), %%rbp\n\t"
 		"addq $16, %%rsp\n"
-	: "=b"(ret)
+	:
 	:
 	: );
 
-	printf("\nret: %ld\n", ret);
 	return 0;
 }
