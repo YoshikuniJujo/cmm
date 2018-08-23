fd9aeeeb89312c0d6785c4f51259502ed458b8d6
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-08-23 12:28:38 +0900

    qiita: brainf-cmm: show return value

--- qiita/brainf-cmm/brainf-cmm/call_cmm.c
+++ qiita/brainf-cmm/brainf-cmm/call_cmm.c
@@ -3,6 +3,8 @@
 int
 main(int argc, char *argv[])
 {
+	unsigned char r;
+
 	__asm__(
 		"addq $-16, %%rsp\n\t"
 		"movq %%rbp, 8(%%rsp)\n\t"
@@ -12,9 +14,10 @@ main(int argc, char *argv[])
 	"ret:\n\t"
 		"movq 8(%%rsp), %%rbp\n\t"
 		"addq $16, %%rsp\n"
-	:
+	: "=b"(r)
 	:
 	: );
 
+	printf("return value: %d\n", r);
 	return 0;
 }
