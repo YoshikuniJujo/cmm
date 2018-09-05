b97cf0eecc78fc8ae7ac177cad3a2fc48157e61b
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-09-05 15:55:36 +0900

    cmm-use-ghc: add CmmGraph

--- check/cmm_no_file/cmm-use-ghc/src/CheckCmmGroup.hs
+++ check/cmm_no_file/cmm-use-ghc/src/CheckCmmGroup.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module CheckCmmGroup where
@@ -12,6 +13,11 @@ import PprCmmDecl
 import PprCmm
 import Hoopl.Label
 import Hoopl.Collections
+import Hoopl.Graph
+import Hoopl.Block
+import Hoopl.Label
+import BlockId
+import Unique
 
 import Tools
 
@@ -24,9 +30,9 @@ out = output dflags
 helloL :: CLabel
 helloL = mkCmmCodeLabel (thisPackage dflags) "hello"
 
-sample1 :: GenCmmGroup CmmStatics CmmTopInfo Char
+sample1 :: GenCmmGroup CmmStatics CmmTopInfo CmmGraph
 sample1 = [
-	CmmProc myTopInfo helloL [] 'z',
+	CmmProc myTopInfo helloL [] cmmGraph1,
 	CmmData (Section Text helloL) statics1
 	]
 
@@ -43,3 +49,21 @@ myStackInfo = StackInfo {
 	arg_space = 0,
 	updfr_space = Nothing,
 	do_layout = False }
+
+entry1 :: CmmNode C O
+entry1 = CmmEntry (uniqueToLbl 123) GlobalScope
+
+branch1 :: CmmNode O C
+branch1 = CmmBranch (uniqueToLbl 321)
+
+block1 :: Block CmmNode C C
+block1 = BlockCC
+	entry1
+	(BMiddle $ CmmAssign (CmmGlobal Sp) (CmmLit $ CmmInt 654 W8))
+	branch1
+
+graph1 :: Graph CmmNode C C
+graph1 = GMany NothingO (mapSingleton (uniqueToLbl 456) block1) NothingO
+
+cmmGraph1 :: CmmGraph
+cmmGraph1 = CmmGraph (mkBlockId $ getUnique (456 :: Int)) graph1
