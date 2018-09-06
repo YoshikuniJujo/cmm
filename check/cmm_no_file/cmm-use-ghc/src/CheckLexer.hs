1455d2919f5cb98552bff35fa5d6fe01849438de
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-09-06 11:41:07 +0900

    cmm-use-ghc: add CheckLexer: test P monad

---
+++ check/cmm_no_file/cmm-use-ghc/src/CheckLexer.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings, LambdaCase #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module CheckCmmLexer where
+
+import Prelude hiding (lex)
+
+import Lexer
+import SrcLoc
+import StringBuffer
+import Outputable
+
+import Tools
+
+lex :: PState -> ParseResult (Located Token)
+lex = unP $ lexer True pure
+
+lexAll :: PState -> ParseResult [Located Token]
+lexAll = unP go
+	where
+	go = do
+		t <- lexer True pure
+		case t of
+			L _ ITeof -> return []
+			_ -> (t :) <$> go
+
+myPState :: PState
+myPState = mkPState
+	dflags0
+	(stringToStringBuffer "main = putStrLn \"hello\"")
+	(mkRealSrcLoc "foo.hs" 0 0)
+
+getResult :: ParseResult a -> Maybe a
+getResult = \case POk _ x -> Just x; _ -> Nothing
+
+outResult :: Outputable a => ParseResult a -> IO ()
+outResult = out . getResult
